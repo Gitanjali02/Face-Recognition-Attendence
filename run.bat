9d650da168d341d1e24e3238ace8e688e343d4b9
Gitanjali02 <20BCS4046@cuchd.in>
2024-09-05 17:45:41 +0530

    Face Recognition Attendence System

---
+++ run.bat
@@ -0,0 +1,8 @@
+python "C:\Users\priya\Downloads\Microsoft Engage\Microsoft Engage\main.py"
+
+dekh rhi n
+haaa
+@REM cmd mein kaam ye to powershell mein hi agr cmd kholna ho to cmd type krke enter press krdena
+ho gya thik jao soo jao ab haaall the best kal ke liye same to youmera to tai h singla ka h kal hahah oye suno n singla sara cc ka submission dekh rha suru se last kr lena viva mein haa uske office me jake dikhana h sara hackerrank pr jo kiye ho 10 tk ohhh submit kr lo aajab choro dekhte h 10kb h tmharakal time subah 11:15 acha mail id agr chagiye to le lena hmara submit h worksheetn to kr diye a call
+@REM step 1:  - > ye terminal mein type krna h 
+@REM step 2: python main.py -> temrinal mein aur uske baad jo link ayega usko copy paste krdena browser mein ya ctrl + click krna okkk sukriya koi na hehe
